5dcf818b37dfd134f920f65d0b61f8bfb9488999
Alejandro González <aledagonale@gmail.com>
2022-05-20 18:12:09 -0300

    Added parameters for message and trigger

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # RickRollJS
+
 This is a simple script that integrates a RickRoll anywhere that accepts HTML and JavaScript, it's prepared to be an 'include and run' script with some references to videogames because why not?
 
 To use, ypu can clone the project, download the file or even link the file from https://saleck3.github.io/RickRollJS/WheneverYouNeedSomebody.js (Thanks github!!!)
+
+To use a different trigger or to set a message, add it as a parameter in the HTML
+
+E.g.:
+
+`<script src="WheneverYouNeedSomebody.js" id="Rick" trigger="asdf" bait="If you're from Konami & see this, please do not sue us for the easter egg"></script>`
+
+-   id="Rick" (required for special message/trigger)
+-   trigger="asdf" Set the key press for the trigger
+-   bait="asdf" Set a message to show in console
--- WheneverYouNeedSomebody.js
+++ WheneverYouNeedSomebody.js
@@ -1,14 +1,29 @@
 window.onload = function trigger() {
-    //A catchy phrase to bait unsuspicious people
-    console.log("If you're from Konami & see this, please do not sue us for the easter egg");
-    //Ad variable to store trigger
+    //Set the trigger (default, the konami code)
+    var trigger =
+        "ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightba";
+
+    var html_element = document.getElementById("Rick");
+    if (html_element != null) {
+        //A catchy phrase to bait unsuspicious people
+
+        if (html_element.getAttribute("bait") != null) {
+            trigger = html_element.getAttribute("bait");
+        }
+
+        //Change the trigger if specified
+        if (html_element.getAttribute("trigger") != null) {
+            trigger = html_element.getAttribute("trigger");
+        }
+    }
+
+    //Add variable to store trigger
     localStorage.setItem("KC", " ");
     //Listen for inputs
     document.addEventListener("keydown", function (e) {
         //Add the new keyPress to stack
         let updated = localStorage.getItem("KC") + e.key;
-        //Set the trigger (default, the konami code)
-        let trigger = "ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightba";
+
         if (updated.includes(trigger)) {
             //If trigger met, execute order 66
             localStorage.removeItem("KC");
@@ -21,40 +36,40 @@ window.onload = function trigger() {
 };
 
 function rickRoll() {
-
     //Create CSS class
-    var style = document.createElement('style');
-    style.type = 'text/css';
+    var style = document.createElement("style");
+    style.type = "text/css";
     style.innerHTML =
-        '.rick {' +
-        '    z-index: 1000;' +
-        '    width: 1060px;' +
-        '    height: 615px;' +
-        '    position: absolute;' +
-        '    top: 50%;' +
-        '    left: 50%;' +
-        '    transform: translate(-50%,-50%);' +
-        '    -ms-transform: translate(-50%,-50%);' +
-        '}';
-    document.getElementsByTagName('head')[0].appendChild(style);
+        ".rick {" +
+        "    z-index: 1000;" +
+        "    width: 1060px;" +
+        "    height: 615px;" +
+        "    position: absolute;" +
+        "    top: 50%;" +
+        "    left: 50%;" +
+        "    transform: translate(-50%,-50%);" +
+        "    -ms-transform: translate(-50%,-50%);" +
+        "}";
+    document.getElementsByTagName("head")[0].appendChild(style);
 
     //Create container
     let Cooldiv = document.createElement("div");
-    Cooldiv.setAttribute('id', 'Rick');
+    Cooldiv.setAttribute("id", "Rick");
     Cooldiv.classList.add("rick");
 
-
     //Make the Iframe with the video
-    let videoIframe = document.createElement('iframe');
-    videoIframe.setAttribute('id', 'Astley');
-    videoIframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture');
-    videoIframe.setAttribute('allowfullscreen', '');
+    let videoIframe = document.createElement("iframe");
+    videoIframe.setAttribute("id", "Astley");
+    videoIframe.setAttribute(
+        "allow",
+        "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    );
+    videoIframe.setAttribute("allowfullscreen", "");
     videoIframe.classList.add("rick");
     //Link to video + '?autoplay=1' (this is important for the video to start on load)
     videoIframe.src = "https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1";
     //Why not another reference
-    videoIframe.setAttribute('title', 'Cake is a lie');
-
+    videoIframe.setAttribute("title", "Cake is a lie");
 
     //Put the iframe in the container
     Cooldiv.appendChild(videoIframe);
